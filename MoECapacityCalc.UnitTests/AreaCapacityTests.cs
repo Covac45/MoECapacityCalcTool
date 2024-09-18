832b0538f1cca7a5fbaa078f095af339ae1b934b
Covac45 <Covac45@live.co.uk>
2024-09-18 21:52:31 +0100

    Discounted Exit Capacity calcs provided for Area abstraction. Requires review of final exits in stairs.
    Tests passing for current implementation.

---
+++ MoECapacityCalc.UnitTests/AreaCapacityTests.cs
@@ -0,0 +1,49 @@
+﻿using MoECapacityCalc.Exits.Datastructs;
+using MoECapacityCalc.Exits;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MoECapacityCalc.Stairs;
+using MoECapacityCalc.Areas;
+
+
+namespace MoECapacityCalc.UnitTests
+{
+    public class AreaCapacityTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        //Merging flow capacity tests
+        [TestCase(280)]
+        public void AreaExitCapacityTest(double expectedExitCapacity)
+        {
+            Exit exit1 = new Exit(ExitType.storeyExit, DoorSwing.with, 1050);
+            Exit exit2 = new Exit(ExitType.finalExit, DoorSwing.with, 1050);
+            Exit exit3 = new Exit(ExitType.finalExit, DoorSwing.against, 1050);
+
+            List<Exit> exits = new List<Exit>();
+            exits.Add(exit1);
+            exits.Add(exit2);
+            exits.Add(exit3);
+
+            Stair stair1 = new Stair(1000, 3, exit1);
+            Stair stair2 = new Stair(1000, 3, exit2);
+
+            List<Stair> stairs = new List<Stair>();
+            stairs.Add(stair1);
+            stairs.Add(stair2);
+
+            Area area = new Area(exits, stairs);
+
+            double exitCapacity = area.CalcDiscountedExitCapacity();
+            Assert.That(exitCapacity, Is.EqualTo(expectedExitCapacity));
+        }
+
+    }
+}
---
+++ MoECapacityCalc/Areas/Area.cs
@@ -0,0 +1,36 @@
+﻿using MoECapacityCalc.Exits;
+using MoECapacityCalc.Stairs;
+using MoECapacityCalc.Stairs.StairFinalExits;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoECapacityCalc.Areas
+{
+    public class Area
+    {
+        public List<Exit> Exits;
+        public List<Stair> Stairs;
+
+        public Area(List<Exit> exits, List<Stair> stairs)
+        {
+            Exits = exits;
+            Stairs = stairs;
+        }
+
+        public double CalcDiscountedExitCapacity()
+        {
+            List<double> exitCapacities = new List<double>();
+
+            foreach (Exit anExit in Exits)
+            {
+                exitCapacities.Add(anExit.CalcExitCapacity());
+            }
+
+            double discountedExitCapacity = exitCapacities.Sum() - exitCapacities.Max();
+            return discountedExitCapacity;
+        }
+    }
+}
